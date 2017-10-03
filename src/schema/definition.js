69820ea2d01dfc1c7efc5ca52cffc015eb5ee282
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2017-10-03 03:34:13 -0400

    Add latest DM schema

--- src/schema/definition.js
+++ src/schema/definition.js
@@ -1,24 +1,125 @@
-const certificateManagementSchema = {
-  certificates: {
-    type: "object",
+const appsSchema = {
+  apps: {
+    type: 'object',
+    properties: {
+      '?': {
+        'store': { type: 'boolean' },
+        'nonStore': { type: 'boolean' }
+      }
+    },
     patternProperties: {
-      ".*": { type: "string" }
+      '.*': {
+        type: 'object',
+        additionalProperties: false,
+        properties: {
+          'pkgFamilyName': { type: 'string' },
+          'version': {
+            type: 'string',
+            oneOf: [
+              { enum: ['?', 'not installed'] },
+              { pattern: '^[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$' }
+            ]
+          },
+          'startUp': {
+            type: 'string',
+            enum: ['none', 'foreground', 'background']
+          },
+          'appxSource': { type: 'string' },
+          'depsSources': { type: 'string' },
+          'certSource': { type: 'string' },
+          'certStore': { type: 'string' }
+        }
+      }
     }
   }
 }
 
-const rebootManagementSchema = {
+const certificatesSchema = {
+  certificates: {
+    type: 'object',
+    patternProperties: {
+      '.+': { type: 'string' }
+    }
+  }
+}
+
+const deviceHealthAttestationSchema = {
+  deviceHealthAttestation: {
+    type: 'object',
+    properties: {
+      'Endpoint': {
+        type: 'string',
+        format: 'uri'
+      },
+      'ReportIntervalInSeconds': {
+        type: 'string',
+        pattern: '^[0-9]+$'
+      }
+    }
+  }
+}
+
+const eventTracingCollectorsSchema = {
+  eventTracingCollectors: {
+    type: 'object',
+    properties: {
+      '?': {
+        type: 'string',
+        enum: ['detailed', 'minimal', 'none']
+      }
+    },
+    patternProperties: {
+      '.+': {
+        type: 'object',
+        properties: {
+          reportProperties: { enum: ['yes', 'no'] },
+          applyProperties: {
+            oneOf: [
+              { const: 'no' },
+              {
+                type: 'object',
+                properties: {
+                  traceLogFileMode: { enum: ['sequential', 'circular'] },
+                  logFileSizeLimitMB: {
+                    type: 'integer',
+                    minimum: 1,
+                    maximum: 2048
+                  },
+                  logFileFolder: { type: 'string' },
+                  started: { enum: ['yes', 'no'] },
+                  patternProperties: {
+                    '.+': {
+                      type: 'object',
+                      properties: {
+                        traceLevel: { enum: ['critical', 'error', 'warning', 'information', 'verbose'] },
+                        keywords: { type: 'string' },
+                        enabled: { type: 'boolean' },
+                        type: { const: 'provider' }
+                      }
+                    }
+                  }
+                }
+              }
+            ]
+          }
+        }
+      }
+    }
+  }
+}
+
+const rebootInfoSchema = {
   rebootInfo: {
-    type: "object",
+    type: 'object',
     additionalProperties: false,
     properties: {
       dailyRebootTime: {
-        type: "string",
-        format: "date-time"
+        type: 'string',
+        format: 'date-time'
       },
       singleRebootTime: {
-        type: "string",
-        format: "date-time"
+        type: 'string',
+        format: 'date-time'
       }
     }
   }
@@ -26,105 +127,194 @@ const rebootManagementSchema = {
 
 const timeInfoSchema = {
   timeInfo: {
-    type: "object",
+    type: 'object',
     additionalProperties: false,
     properties: {
-      timeZoneDaylightBias: { type: "integer" },
+      timeZoneDaylightBias: { type: 'integer' },
       timeZoneDaylightDate: {
-        type: "string",
-        format: "date-time"
+        type: 'string',
+        format: 'date-time'
       },
-      timeZoneDaylightName: { type: "string" },
-      timeZoneStandardBias: { type: "integer" },
+      timeZoneDaylightName: { type: 'string' },
+      timeZoneDaylightDayOfWeek: {
+        type: 'integer',
+        minimum: 0,
+        maximum: 6
+      },
+      timeZoneStandardBias: { type: 'integer' },
       timeZoneStandardDate: {
-        type: "string",
-        format: "date-time"
+        type: 'string',
+        format: 'date-time'
       },
-      timeZoneStandardName: { type: "string" },
-      timeZoneBias: { type: "integer" },
+      timeZoneStandardName: { type: 'string' },
+      timeZoneStandardDayOfWee: {
+        type: 'integer',
+        minimum: 0,
+        maximum: 6
+      },
+      timeZoneBias: { type: 'integer' },
       ntpServer: {
-        type: "string",
-        format: "uri"
+        type: 'string',
+        format: 'uri'
       }
+    }
+  }
+}
+
+const timeServiceSchema = {
+  timeService: {
+    type: 'object',
+    properties: {
+      enabled: { enum: ['yes', 'no'] },
+      startup: { enum: ['auto', 'manual'] },
+      started: { enum: ['yes', 'no'] },
+      sourcePriority: { enum: ['local', 'remote'] }
+    }
+  }
+}
+
+const wifiSchema = {
+  wifi: {
+    oneOf: [
+      { enum: ['no-apply-no-report', 'no-apply-yes-report'] },
+      {
+        type: 'object',
+        additionalProperties: false,
+        properties: {
+          applyProperties: {
+            oneOf: [
+              { const: 'no' },
+              {
+                type: 'object',
+                patternProperties: {
+                  '.+': {
+                    oneOf: [
+                      { const: 'uninstall' },
+                      {
+                        type: 'object',
+                        additionalProperties: false,
+                        properties: {
+                          'profile': { type: 'string' }
+                        }
+                      }
+                    ]
+                  }
+                }
+              }
+            ]
+          },
+          reportProperties : { enum: ['yes', 'no'] }
+        }
+      }
+    ]
+  }
+}
+
+const windowsTelemetrySchema = {
+  windowsTelemetry: {
+    type: 'object',
+    additionalProperties: false,
+    properties: {
+      level: { enum: ['security', 'basic', 'enhanced', 'full'] }
     }
   }
 }
 
 const windowsUpdatePolicySchema = {
   windowsUpdatePolicy: {
-    type: "object",
+    oneOf: [
+      { enum: ['no-apply-no-report', 'no-apply-yes-report'] },
+      {
+        type: 'object',
+        additionalProperties: false,
+        properties: {
+          applyProperties: {
+            type: 'object',
+            additionalProperties: false,
+            properties: {
+              activeHoursStart: {
+                type: 'integer',
+                minimum: 0,
+                maximum: 23
+              },
+              activeHoursEnd: {
+                type: 'integer',
+                minimum: 0,
+                maximum: 23
+              },
+              allowAutoUpdate: { type: 'boolean' },
+              allowUpdateService: { type: 'boolean' },
+              branchReadinessLevel:  {
+                type: 'integer',
+                minimum: 2,
+                maximum: 32
+              },
+              deferFeatureUpdatesPeriod: {
+                type: 'integer',
+                minimum: 0,
+                maximum: 365
+              },
+              deferQualityUpdatesPeriod: {
+                type: 'integer',
+                minimum: 0,
+                maximum: 30
+              },
+              pauseFeatureUpdates: { type: 'boolean' },
+              pauseQualityUpdates: { type: 'boolean' },
+              requireUpdateApproval: { type: 'boolean' },
+              scheduledInstallDay: {
+                type: 'integer',
+                minimum: 0,
+                maximum: 7
+              },
+              scheduledInstallTime: {
+                type: 'integer',
+                minimum: 0,
+                maximum: 23
+              },
+              ring: { enum: ['EarlyAdopter', 'Preview', 'GeneralAvailability'] },
+              sourcePriority: { enum: ['local', 'remote'] }
+            }
+          },
+          reportProperties: {
+            enum: ['yes', 'no']
+          }
+        }
+      }
+    ]
+  }
+}
+
+const windowsUpdatesSchema = {
+  windowsUpdates: {
+    type: 'object',
     additionalProperties: false,
     properties: {
-      activeHoursStart: {
-        type: "integer",
-        minimum: 0,
-        maximum: 23
-      },
-      activeHoursEnd: {
-        type: "integer",
-        minimum: 0,
-        maximum: 23
-      },
-      allowAutoUpdate: { type: "boolean" },
-      allowMUUpdateService: { type: "boolean" },
-      allowNonMicrosoftSignedUpdate: { type: "boolean" },
-      allowUpdateService: { type: "boolean" },
-      branchReadinessLevel:  {
-        type: "integer",
-        minimum: 2,
-        maximum: 32
-      },
-      deferFeatureUpdatesPeriod: {
-        type: "integer",
-        minimum: 0,
-        maximum: 365
-      },
-      deferQualityUpdatesPeriod: {
-        type: "integer",
-        minimum: 0,
-        maximum: 30
-      },
-      excludeWUDrivers: { type: "boolean" },
-      pauseFeatureUpdates: { type: "boolean" },
-      pauseQualityUpdates: { type: "boolean" },
-      requireUpdateApproval: { type: "boolean" },
-      scheduledInstallDay: {
-        type: "integer",
-        minimum: 0,
-        maximum: 7
-      },
-      scheduledInstallTime: {
-        type: "integer",
-        minimum: 0,
-        maximum: 23
-      },
-      updateServiceUrl: {
-        type: "string",
-        format: "uri"
-      }
+      approved: { type: 'string' }
     }
   }
 }
 
 export const desiredPropertiesSchema = {
-  type: "object",
+  type: 'object',
   properties: {
-    desired: {
-      type: "object",
+    windows: {
+      type: 'object',
       properties: {
-        microsoft: {
-          type: "object",
+        ...appsSchema,
+        ...certificatesSchema,
+        ...eventTracingCollectorsSchema,
+        ...rebootInfoSchema,
+        ...timeInfoSchema,
+        ...timeServiceSchema,
+        ...windowsTelemetrySchema,
+        ...windowsUpdatePolicySchema,
+        ...windowsUpdatesSchema,
+        management: {
+          type: 'object',
           properties: {
-            management: {
-              type: "object",
-              additionalProperties: false,
-              properties: {
-                ...certificateManagementSchema,
-                ...rebootManagementSchema,
-                ...timeInfoSchema,
-                ...windowsUpdatePolicySchema
-              }
-            }
+            ...deviceHealthAttestationSchema,
+            ...wifiSchema
           }
         }
       }
